5082f27fff3d95483f8608682187cb035f91dd40
Elyana Quah <elyanaqwc@gmail.com>
2025-03-29 13:53:32 -0500

    make changes to server

---
+++ backend/schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+from pydantic_extra_types.phone_numbers import PhoneNumber
+from typing import Annotated, Union
+import phonenumbers
+
+
+PhoneNumberValidator = Annotated[Union[str, phonenumbers.PhoneNumber], 
+                                PhoneNumber(default_region='US', supported_regions=['US'])]
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: EmailStr
+    phone: PhoneNumberValidator
+
